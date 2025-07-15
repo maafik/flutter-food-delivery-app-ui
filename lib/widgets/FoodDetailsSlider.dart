e3ee578ede0b8a2dbecda9f8d0310508ffc6e493
Ярослав Сосницких <maafik@66.ru>
2025-07-15 08:51:47 +0300

    322

--- lib/widgets/FoodDetailsSlider.dart
+++ lib/widgets/FoodDetailsSlider.dart
@@ -1,4 +1,4 @@
-import 'package:carousel_pro/carousel_pro.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class FoodDetailsSlider extends StatelessWidget {
@@ -13,37 +13,44 @@ class FoodDetailsSlider extends StatelessWidget {
       @required this.slideImage3})
       : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 10, right: 10),
-      child: Container(
-          child: Carousel(
-            images: [
-              Image.asset(
-                slideImage1,
-              ),
-              /* Image.asset(
-                slideImage2,
-                height: double.infinity,
-                width: double.infinity,
-              ),
-              Image.asset(
-                slideImage3,
-                height: double.infinity,
-                width: double.infinity,
-              ),*/
-            ],
-            dotSize: 4.0,
-            dotSpacing: 15.0,
-            dotColor: Colors.purple,
-            indicatorBgPadding: 5.0,
-            dotBgColor: Colors.black54.withOpacity(0),
-            borderRadius: true,
-            radius: Radius.circular(20),
-            moveIndicatorFromBottom: 180.0,
-            noRadiusForIndicator: true,
-          )),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+  return Padding(
+    padding: EdgeInsets.only(left: 10, right: 10),
+    child: Container(
+      child: CarouselSlider(
+        items: [
+          Image.asset(
+            slideImage1,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          // Можно добавить ещё слайды
+          /*
+          Image.asset(
+            slideImage2,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          Image.asset(
+            slideImage3,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          */
+        ],
+        options: CarouselOptions(
+          height: 200, // установи подходящую высоту
+          enlargeCenterPage: true,
+          enableInfiniteScroll: true,
+          autoPlay: true,
+          autoPlayInterval: Duration(seconds: 3),
+          autoPlayAnimationDuration: Duration(milliseconds: 800),
+          autoPlayCurve: Curves.fastOutSlowIn,
+          viewportFraction: 0.9,
+        ),
+      ),
+    ),
+  );
 }
+
